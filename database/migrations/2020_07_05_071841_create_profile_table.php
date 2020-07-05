edf8b7da5d2854c23ed70fff059680ec991538a1
Cakra <cakrawisnumurthy@gmail.com>
2020-07-05 20:19:33 +0700

    quiz 3

---
+++ database/migrations/2020_07_05_071841_create_profile_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProfileTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profile', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('users_id')->references('id')->on('users');
+            $table->integer('artikel_id')->unsigned();
+            $table->foreign('artikel_id')->references('id')->on('artikel');
+            $table->string('nama_lengkap');
+            $table->string('photo');
+            $table->string('email')->unique();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profile');
+    }
+}
---
+++ database/migrations/2020_07_05_071924_create_follow_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFollowTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('follow', function (Blueprint $table) {
+            $table->string('user_username');
+            $table->foreign('user_username')->references('username')->on('users');
+            $table->integer('followers_id');
+            $table->foreign('followers_id')->references('id')->on('followers');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('follow');
+    }
+}
